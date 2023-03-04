5087b98685766ad2c262195380a449831282f591
DavidBAMBA <dbambague@unal.edu.co>
2023-03-03 21:27:24 -0500

    Graficas completas

--- R-W-inverse_problem.py
+++ R-W-inverse_problem.py
@@ -4,31 +4,30 @@ from deepxde.backend import tf
 import matplotlib.pyplot as plt
 
 #parametros a encontrar
-w_r = dde.Variable(2.0) # Variable para ajustar
+w_r = dde.Variable(1.0) # Variable para ajustar
 w_i = dde.Variable(-1.0) # Variable para ajustar
-#frecuencia como suma de real e imaginaria
-#W2 = w_r**2 + w_i**2
+bdr_inf = 0.9
 
 #funciones para extraer valores de la sulucion y agregarlos al 
 #entrenamiento para ajustar las w
 def gen_traindata_r(N):
-    yvals = (np.linspace(-0.9, 0.9, N)).reshape(N, 1)
+    yvals = (np.linspace(-bdr_inf, bdr_inf, N)).reshape(N, 1)
     phi_vals = func_r(yvals)
     return yvals, phi_vals
 
 def gen_traindata_i(N):
-    yvals = (np.linspace(-0.9, 0.9, N)).reshape(N, 1)
+    yvals = (np.linspace(-bdr_inf, bdr_inf, N)).reshape(N, 1)
     phi_vals = func_i(yvals)
     return yvals, phi_vals
 
 #Soluciones analiticas real e imaginaria 
 def func_r(y):
     x = tf.math.atanh(y)
-    return tf.math.sqrt(tf.cosh(x)) * tf.math.cos(tf.math.log(tf.math.cosh(x))) #* np.sinh(y)
+    return tf.math.sqrt(tf.math.cosh(x)) * tf.math.cos(tf.math.log(tf.math.cosh(x))) #* np.sinh(y)
 
 def func_i(y):
     x = tf.math.atanh(y)
-    return tf.math.sqrt(tf.cosh(x)) * tf.math.sin(tf.math.log(tf.math.cosh(x))) #* np.sinh(y)
+    return tf.math.sqrt(tf.math.cosh(x)) * tf.math.sin(tf.math.log(tf.math.cosh(x))) #* np.sinh(y)
 
 #Ecuacion diferencial
 def PDE(y,x):
@@ -56,25 +55,25 @@ def PDE(y,x):
     return  [f_r, f_i] 
 
 #Definicion del intervalo de solucion
-geom = dde.geometry.Interval(-0.99,0.99)
+geom = dde.geometry.Interval(-bdr_inf,bdr_inf)
 
 #condiciones de frontera
 ic_u = dde.icbc.DirichletBC(geom, func_r,  lambda _,on_boundary: on_boundary, component=0)
 ic_v = dde.icbc.DirichletBC(geom, func_i,  lambda _,on_boundary: on_boundary, component=1)
 
 #datos experimentales para hayar el parametro
-y_values, u_values = gen_traindata_r(500) 
+y_values, u_values = gen_traindata_r(50) 
 exp_data_r = dde.icbc.PointSetBC(y_values, u_values, component=0) # valores w real 
 
-y_values, v_values = gen_traindata_i(500)
+y_values, v_values = gen_traindata_i(50)
 exp_data_i = dde.icbc.PointSetBC(y_values, v_values, component=1) #valores w imaginario
 
 #Datos De entrenamiento
-data = dde.data.PDE(geom, PDE, [ic_u, ic_v, exp_data_r, exp_data_i], 1000, 200, num_test=100,
+data = dde.data.PDE(geom, PDE, [ic_u, ic_v, exp_data_r, exp_data_i], 100, 100, num_test=60,
                     train_distribution='pseudo', anchors=y_values)
 
 #Red neuronal
-layer_size = [1] + [100] * 4 + [2]
+layer_size = [1] + [20] * 3 + [2]
 activation = 'tanh'
 initializer = "Glorot normal"
 net = dde.nn.FNN(layer_size, activation, initializer)
@@ -84,34 +83,59 @@ model = dde.Model(data, net)
 variable = dde.callbacks.VariableValue([w_r,w_i], period=1000)
 
 # Optimizador Adam para ajustar el valor de w^2
+#pde_resampler = dde.callbacks.PDEPointResampler(period=100)
+
 model.compile("adam", lr=1e-3, external_trainable_variables=[w_i,w_r])
-loss_history, train_state = model.train(iterations=14000, callbacks=[variable])
+loss_history, train_state = model.train(iterations=20000, callbacks=[variable])
 
 # Optimizador L-BFGS para ajustar la ecuación diferencial (no ajusta w^2)
 dde.optimizers.config.set_LBFGS_options(maxiter=10000)
 model.compile("L-BFGS", external_trainable_variables=[w_i,w_r])
 losshistory, train_state = model.train(callbacks=[variable])
 
+#Parametros calculados
+W_r_esp = 1/2
+W_i_esp = -1/2
+print('Esperados: ','W_r_est = ',W_r_esp, "  W_i_est =", W_i_esp)
+w_r_est, w_i_est = variable.get_value()
+print('Predichos: ','w_r_ = ',w_r_est, "  w_i =", w_i_est)
+
+
 #Graficas
 dde.utils.external.plot_loss_history(losshistory)
 
-t = geom.uniform_points(1000)
-y = model.predict(t)
-W_exp = 1/2 # w= \pm 1 -i(n+1/2) usando n=0 por que si :V
+x = geom.uniform_points(1000)
+y = model.predict(x)
+
+u_pred, v_pred = y[:, 0:1], y[:, 1:2]
+
+with tf.Session() as sess: #pasar el tensor a arreglo de numpy
+    array_u = sess.run(func_r(x))
+    array_v = sess.run(func_i(x))
+
+f_u = np.array(array_u)
+f_v = np.array(array_v)
 
 plt.figure()
-plt.plot(t, y[:, 0],"-", label="real_solution")
-plt.plot(t, y[:, 1],"-", label="imaginary solution")
+plt.plot(x, f_u, "-", label="u_true")
+plt.plot(x, u_pred, "--", label="u_pred")
 
-#plt.plot(t, func_r,'-',label="Exacta u")
-#plt.plot(t, func_i,'-',label="Exacta v")
-plt.xlabel('x')
-plt.ylabel('y')
+plt.title("Solutions to Regger-Wheeler Quasinormal Modes real")
+plt.xlabel("y")
+plt.ylabel('{\phy}')
 plt.legend()
+plt.grid()
+plt.savefig("Comparison_real_part")
 plt.show()
 
-w_r_est, w_i_est = variable.get_value()
-print('w_r_ = ',w_r_est, "  w_i =", w_i_est)
-W_r_est = 1/2
-W_i_est = -1/2
-print('W_r_est = ',W_r_est, "  W_i_est =", W_i_est)
+plt.figure()
+plt.plot(x, f_v, "-", label="v_true")
+plt.plot(x, v_pred, "--", label="v_pred")
+plt.title("Solutions to Regger-Wheeler Quasinormal Modes imaginary ")
+plt.xlabel("y")
+plt.ylabel('{phy}')
+plt.legend()
+plt.grid()
+plt.savefig("Comparison_imaginary_part")
+plt.show()
+
